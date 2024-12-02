10d1a061a0a78ee946ac8d13a024fe94f53b54d4
Vanessa Rodrigues <varodrig@made-f0Br7s10.clusters.42paris.fr>
2024-12-02 18:26:48 +0100

    -

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,27 +6,11 @@
 /*   By: varodrig <varodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:16:21 by varodrig          #+#    #+#             */
-/*   Updated: 2024/11/26 19:19:17 by varodrig         ###   ########.fr       */
+/*   Updated: 2024/12/02 16:56:09 by varodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	open_pipes(int pipes_nb, int fd[pipes_nb][2])
-{
-	int	i;
-
-	i = 0;
-	while (i < pipes_nb)
-	{
-		if (pipe(fd[i++]) == -1)
-		{
-			//fprintf(stderr, "Error with creating pipe\n");
-			exit(1);
-		}
-		//fprintf(stderr, "%d pipes opened\n", i);
-	}
-}
 
 // we close useless fds OR
 // we close last fds when finished
@@ -48,8 +32,11 @@ void	close_fds(int pipes_nb, int (*fd)[2], int i, bool exec)
 	}
 	else
 	{
-		close(fd[i][0]);
-		close(fd[i + 1][1]);
+		if (pipes_nb != 0)
+		{
+			close(fd[i][0]);
+			close(fd[i + 1][1]);
+		}
 	}
 }
 
@@ -130,7 +117,7 @@ void	create_args(t_exec *temp, int args_nb, char *args[args_nb])
 	t_args	*curr;
 	int		i;
 
-	//fprintf(stderr, "entering create args\n");
+	// fprintf(stderr, "entering create args\n");
 	i = 0;
 	args[i] = temp->cmd;
 	curr = temp->args;
@@ -138,12 +125,12 @@ void	create_args(t_exec *temp, int args_nb, char *args[args_nb])
 	while (curr)
 	{
 		args[i] = curr->value;
-		//fprintf(stderr, "args[%d] : %s\n", i, args[i]);
+		// fprintf(stderr, "args[%d] : %s\n", i, args[i]);
 		curr = curr->next;
 		i++;
 	}
 	args[i] = NULL;
-	//fprintf(stderr, "args[%d] : %s\n", i, args[i]);
+	// fprintf(stderr, "args[%d] : %s\n", i, args[i]);
 }
 
 int	ft_char_count(char *str, char c)
@@ -180,20 +167,20 @@ char	*find_path(char *cmd, t_env *envp)
 	t_env	*curr;
 	int		i;
 
-	//fprintf(stderr, "entered find_path\n");
+	// fprintf(stderr, "entered find_path\n");
 	curr = envp;
 	while (curr)
 	{
 		if (ft_strncmp("PATH=", curr->raw, 5) == 0)
 		{
-			//fprintf(stderr, "PATH found\n");
+			// fprintf(stderr, "PATH found\n");
 			break ;
 		}
 		curr = curr->next;
 	}
-	//fprintf(stderr, "sorti de la boucle curr\n");
+	// fprintf(stderr, "sorti de la boucle curr\n");
 	paths = ft_split(curr->value, ':');
-	//fprintf(stderr, "split done\n");
+	// fprintf(stderr, "split done\n");
 	i = 0;
 	while (paths[i++])
 	{
@@ -203,13 +190,13 @@ char	*find_path(char *cmd, t_env *envp)
 		if (!access(exec, X_OK | F_OK))
 		{
 			ft_free_all(paths);
-			fprintf(stderr, "command returned : %s\n", exec);
+			// fprintf(stderr, "command returned : %s\n", exec);
 			return (exec);
 		}
 		free(exec);
 	}
 	ft_free_all(paths);
-	//fprintf(stderr, "command not found");
+	// fprintf(stderr, "command not found");
 	return (ft_strdup(cmd));
 }
 
@@ -254,7 +241,7 @@ int	ft_execution(t_ctx *ctx, t_exec *temp)
 	char	*args[size_linked_list(temp->args) + 2];
 	char	**envp;
 
-	//fprintf(stderr, "entered ft_execution\n");
+	// fprintf(stderr, "entered ft_execution\n");
 	args_nb = size_linked_list(temp->args) + 2;
 	// execve(path, comd, envp);
 	// char	*args[] = {"/bin/ls", "-l", "/home", NULL};
@@ -266,12 +253,12 @@ int	ft_execution(t_ctx *ctx, t_exec *temp)
 	// 	n++;
 	// }
 	envp = envp_format(ctx->envp);
-	//fprintf(stderr, "envp copied\n");
+	// fprintf(stderr, "envp copied\n");
 	if (execve(temp->cmd, args, envp) == -1)
 	{
-		//fprintf(stderr, "not absolute link\n");
+		// fprintf(stderr, "not absolute link\n");
 		path = find_path(temp->cmd, ctx->envp);
-		//fprintf(stderr, "path found\n");
+		// fprintf(stderr, "path found\n");
 		if (execve(path, args, envp) == -1)
 		{
 			free(path);
@@ -287,28 +274,18 @@ int	ft_execution(t_ctx *ctx, t_exec *temp)
 
 void	child_process(t_ctx *ctx, int (*fd)[2], int i, t_exec *temp)
 {
-	//fprintf(stderr, "entering child process n.%d\n", i);
-	fflush(stdout);
-	close_fds((ctx->exec_count) + 1, fd, i, false);
-	//fprintf(stderr, "close fds done\n");
-	fflush(stdout);
+	close_fds((ctx->exec_count) - 1, fd, i, false);
 	if (ctx->exec_count > 1)
 	{
 		if (i > 0)
 			dup2(fd[i][0], 0);
 		if (i < ctx->exec_count - 1)
 			dup2(fd[i + 1][1], 1);
-		//fprintf(stderr, "2 dup2 done\n");
 	}
 	exec_handle_redir(temp);
-	//fprintf(stderr, "exec_handle_redir done\n");
 	if (ft_execution(ctx, temp) == -1)
-	{
-		//fprintf(stderr, "Error with exec in process %d\n", i);
 		exit(1);
-	}
 	close_fds((ctx->exec_count) + 1, fd, i, true);
-	//fprintf(stderr, "ending child process n.%d", i);
 }
 
 void	ft_wait_all(int childs, int *pid)
@@ -323,39 +300,74 @@ void	ft_wait_all(int childs, int *pid)
 	}
 }
 
-int	exec(t_ctx *ctx)
+void	open_pipes(int pipes_nb, int fd[pipes_nb][2])
 {
-	int		fd[ctx->exec_count + 1][2];
-	int		pids[ctx->exec_count + 1];
-	int		i;
+	int	i;
+
+	i = 0;
+	while (i < pipes_nb)
+	{
+		if (pipe(fd[i++]) == -1)
+		{
+			////fprintf(stderr, "Error with creating pipe\n");
+			exit(1);
+		}
+		// fprintf(stderr, "%d pipes opened\n", i);
+	}
+}
+
+int	exec_parent(t_ctx *ctx)
+{
+	t_exec	*temp;
+	int		fd[ctx->exec_count][2];
 	t_exec	*temp;
 
-	if (strcmp(ctx->exec->cmd, "exit") == 0)
-		exit(1);
-	//fprintf(stderr, "start exec\n");
 	// we open cmd_nb + 1 pipes
-	open_pipes((ctx->exec_count + 1), fd);
-	//fprintf(stderr, "pipes opened\n");
+	open_pipes((ctx->exec_count - 1), fd);
 	temp = ctx->exec;
 	i = 0;
 	while (temp)
 	{
-		//fprintf(stderr, "comd n.%d\n", i);
-		pids[i] = fork();
-		if (pids[i] == -1)
-		{
-			//fprintf(stderr, "Error with creating process\n");
-			return (2);
-		}
-		//fprintf(stderr, "forked\n");
-		if (pids[i] == 0)
-		{
-			//fprintf(stderr, "on est la\n");
-			child_process(ctx, fd, i, temp);
-		}
-		i++;
+		ctx->pids[ctx->pid_count] = fork();
+		if (ctx->pids[ctx->pid_count] == -1)
+			err_fork; //TODO
+		else if (ctx->pids[ctx->pid_count] == 0)
+			child_process(ctx, fd, ctx->pid_count, temp);
+		ctx->pid_count++;
 		temp = temp->next;
 	}
 	ft_wait_all(ctx->exec_count, pids);
+	return (0);
+}
+
+int	exec(t_ctx *ctx)
+{
+	t_exec	*temp;
+
+	// TODO il sert a quoi exit_code ?
+	if (ctx->exec_count == 0)
+		return (0);
+	// store STDIN & STDOUT
+	// TODO
+	set_std(ctx, 0);
+	// 1 command and its a builtin (no loop)
+	temp = ctx->exec;
+	if (ctx->exec_count == 1 && is_builtin(temp->cmd)) // TODO
+	{
+		// if errors in redirections, exit
+		if (err_redirs(temp))
+		{
+			ctx->exit_code = 1;
+			return (ctx->exit_code);
+		}
+		// id exit, printf "exit" ?? (dans la bi_exit?)
+		// unlink (close) all the N_HEREDOC(unlik dans le else)
+		unlink_all(ctx);
+		// TODO// fonction reutilisee dans le else
+		ctx->exit_code = do_builtin(ctx, temp->cmd, temp->args); // TODO
+		return (ctx->exit_code);
+	}
+	exec_parent(ctx);
+	set_std(ctx, 0);
 	return (0);
 }
